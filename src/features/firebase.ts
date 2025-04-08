e2c8fe0174bd7f11cf8167c9f8b6743ce20a1e09
aadii-19 <adithpramod197@gmail.com>
2025-04-08 21:36:34 +0530

    Moved Firebase config to .env and updated packages

--- src/features/firebase.ts
+++ src/features/firebase.ts
@@ -2,19 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7ShhHskYRJwBhTKLstlYOfaZ0hjwb0CY",
-  authDomain: "guardian-gate-fd812.firebaseapp.com",
-  projectId: "guardian-gate-fd812",
-  storageBucket: "guardian-gate-fd812.appspot.com",
-  messagingSenderId: "52177565301",
-  appId: "1:52177565301:web:a2433f75317f5d3ea42a3c",
-  measurementId: "G-2J5Y5XPKTP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ Initialize Auth and Google provider
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider(); // 💥 THIS was missing!
+export const provider = new GoogleAuthProvider();
